500e24a8e7fb8210667fbfa55b26b8e2e75a2be0
Ali Saim <alisaimwindows@gmail.com>
2017-07-20 18:39:34 -0400

    First Commit

---
+++ LetMeHmmm/wwwroot/js/site.js
@@ -0,0 +1,21 @@
+﻿// Write your Javascript code.
+function reply_click(clicked_id)
+{   
+    var i1 = 3;
+    var i2 = 3;
+
+    switch(clicked_id){
+    case "image1":
+        document.getElementById("image1").src="/images/banner"+i1+".svg";
+        console.log("First Image");
+        break;
+    case "image2":
+        document.getElementById("image2").src="/images/banner"+i2+".svg";
+        console.log("Second Image");
+        break;
+    case 6:
+        day = "Saturday";
+
+    alert(clicked_id);
+    }
+}
---
+++ LetMeHmmm/wwwroot/js/testJS.js
@@ -0,0 +1,2 @@
+﻿
+console.log("my test js file...wordk?");
